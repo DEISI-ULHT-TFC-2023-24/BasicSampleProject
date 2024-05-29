e1eefe7d9f8838a009f7f56a8da776144e3cf218
Tomas-21907834 <tomasmartins2@gmail.com>
2024-05-29 15:16:25 +0100

    atualizado

--- AppExamples/BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/BatteryYoutubeDrainTest.java
+++ AppExamples/BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/BatteryYoutubeDrainTest.java
@@ -12,22 +12,26 @@ public class BatteryYoutubeDrainTest {
 
     private static final String YOUTUBE_VIDEO_ID = "dQw4w9WgXcQ";
     private static final String YOUTUBE_PACKAGE_NAME = "com.google.android.youtube";
-    public static final long WATCH_DURATION = BatteryDrainTestActivity.WATCH_DURATION+1000;
+    public static final long WATCH_DURATION = BatteryDrainTestActivity.WATCH_DURATION + 1000;
+    private static final long DELAY_BETWEEN_TESTS = 5000; // 5 seconds
 
     @Rule
     public ActivityScenarioRule<BatteryDrainTestActivity> activityScenarioRule =
             new ActivityScenarioRule<>(BatteryDrainTestActivity.class);
 
     @Before
-    public void setUp() {
+    public void setUp() throws InterruptedException {
+        // Delay before each test
+        Thread.sleep(DELAY_BETWEEN_TESTS);
         // Initialize Intents for testing Intent interactions
         Intents.init();
     }
 
     @After
-    public void tearDown() {
+    public void tearDown() throws InterruptedException {
         // Release Intents after each test
         Intents.release();
+        Thread.sleep(DELAY_BETWEEN_TESTS);
     }
 
     @Test
